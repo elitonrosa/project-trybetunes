1a3bd71a6ec9f31c507aac35205158f6465cbca1
Eliton da Rosa <elitondr@live.com>
2023-01-20 17:04:49 -0300

    Requisito 5 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchInput: '',
+    disableButton: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.enableButton());
+  };
+
+  enableButton = () => {
+    const { searchInput } = this.state;
+    const two = 2;
+    if (searchInput.length >= two) {
+      this.setState({ disableButton: false });
+    } else {
+      this.setState({ disableButton: true });
+    }
+  };
+
   render() {
+    const { disableButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div>
+          <input
+            type="text"
+            name="searchInput"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disableButton }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }

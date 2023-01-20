666e484a5982f89e9de499ffb28df902e2a0ebd9
Eliton da Rosa <elitondr@live.com>
2023-01-20 16:47:40 -0300

    Requisito 4 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -22,7 +23,14 @@ export default class Header extends Component {
     const { isLoading, username } = this.state;
     return (
       <div data-testid="header-component">
-        {isLoading ? <Loading /> : <p data-testid="header-user-name">{username}</p>}
+        {isLoading ? <Loading /> : (
+          <>
+            <p data-testid="header-user-name">{username}</p>
+            <Link to="/search" data-testid="link-to-search">Inicio</Link>
+            <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+            <Link to="/profile" data-testid="link-to-profile">Meu Perfil</Link>
+          </>
+        )}
       </div>
     );
   }
